d44d978e11a2986e125bfce239184c38d1a184fd
jhern657 <jhern657@mtroyal.ca>
2021-02-03 13:45:58 -0700

    main

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+
 int main( ) {
 
     char str[100];
 
     printf( "Enter a value :");
-    gets( str );
-
-    printf( "\nYou entered: ");
+    scanf( "%s",str );
+    
+    printf( "You entered: ");
     puts( str );
 
     return 0;
