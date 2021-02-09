0e650fb27b9087eba788e487ed782310c2b63b19
jhern657 <jhern657@mtroyal.ca>
2021-02-08 22:42:04 -0700

    main

--- main.c
+++ main.c
@@ -1,43 +1,16 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <fcntl.h>
 
-void choice(char str[]);
+#include "utilities.h"
 
-int main( ) {
 
-    char str[100];
-
-    printf( "mySH$ ");       /* Command line prompt */
-    scanf( "%s",str );       /* Read in command */
-
-    while(strcmp(str, "exit") != 0)
-    {
-	choice(str);
-	printf( "\nmySH$ ");       /* Command line prompt */
-	scanf( "%s",str );       /* Read in command */
-    }
-    exit(0);
-    
-    //printf( "You entered: "); /* Echo command */
-    //puts( str );
-
-}
-
-void choice(char str[])
+int main()
 {
 
-    if(strcmp(str, "ls") == 0)
-    {
-	printf("inside ls");
-    }
-    else
-    {
-	printf("invalid entry");
-
-    }
-
-    //return;
+    prompt();
+    
+    return 0;
 }
+
+
 	
  
