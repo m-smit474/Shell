85b68191e51e3a232c4683c97810d0a455bfd4cb
jhern657 <jhern657@mtroyal.ca>
2021-02-08 22:36:50 -0700

    utilities.h file

---
+++ utilities.h
@@ -0,0 +1,10 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+
+void prompt();
+void read_write();
+int compare_strings(const char *string1, const char *string2);
+void choice(char command[]);
+
+#endif 
